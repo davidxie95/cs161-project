9594d38152146a34c96d5152775894803e557a98
tien353353 <31874122+tien353353@users.noreply.github.com>
2018-04-28 14:59:42 -0700

    Add files via upload

---
+++ radio_value.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['submit'])) {
+if(isset($_POST['radio']))
+{
+echo "<span>You have selected :<b> ".$_POST['radio']."</b></span><br/>";
+}
+else{ echo "<span>Please choose any radio button.</span>";}
+}
+?>
+
+
+
---
+++ select_value.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])){
+if(!empty($_POST['Currency'])) {
+echo "<span>You have selected :</span><br/>";
+    
+// As output of $_POST['Color'] is an array we have to use Foreach Loop to display individual value
+foreach ($_POST['Currency'] as $select)
+{
+echo "<span><b>".$select."</b></span><br/>";
+}
+}
+else { echo "<span>Please Select Atleast One Currency.</span><br/>";}
+}
+
+?>
